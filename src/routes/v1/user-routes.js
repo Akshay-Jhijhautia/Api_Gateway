413159f6c3521cdae5e7d897bcca69db6d8ae40b
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-06-23 18:43:00 +0530

    added middleware check for auth

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -2,8 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const { UserController } = require("../../controllers");
+const { AuthRequestMiddlewares } = require("../../middlewares");
 
-router.post("/signup", UserController.signUp);
-router.post("/signin", UserController.signIn);
+router.post(
+  "/signup",
+  AuthRequestMiddlewares.validateAuthRequest,
+  UserController.signUp
+);
+router.post(
+  "/signin",
+  AuthRequestMiddlewares.validateAuthRequest,
+  UserController.signIn
+);
 
 module.exports = router;
